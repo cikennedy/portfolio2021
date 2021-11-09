a307e63bca5206418b69b1d480691be31822b4e7
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-11-09 14:41:54 -0600

    added user details, added most components and baseline file structure

---
+++ portfolio/src/User.js
@@ -0,0 +1,67 @@
+import image from './assets/ckphoto.jpeg'
+
+export default {
+    pic: image,
+    name: 'Chris Kennedy',
+    titles: ['Software Engineer', 'Web Developer'],
+    socialProfiles: {
+        github: 'https://github.com/cikennedy',
+        linkedin: 'https://www.linkedin.com/in/cikennedy/'
+    },
+    resumeLink: 'https://github.com/cikennedy/updated-portfolio/raw/main/assets/Resume.pdf',
+    projects: [
+        {
+            image: '',
+            title: '',
+            description: '',
+            projectgithub: '',
+            projectdeployed: ''
+        },
+        {
+            image: '',
+            title: '',
+            description: '',
+            projectgithub: '',
+            projectdeployed: ''
+        },
+        {
+            image: '',
+            title: '',
+            description: '',
+            projectgithub: '',
+            projectdeployed: ''
+        },
+        {
+            image: '',
+            title: '',
+            description: '',
+            projectgithub: '',
+            projectdeployed: ''
+        },
+        {
+            image: '',
+            title: '',
+            description: '',
+            projectgithub: '',
+            projectdeployed: ''
+        },
+        {
+            image: '',
+            title: '',
+            description: '',
+            projectgithub: '',
+            projectdeployed: ''
+        }
+    ],
+    skills: {
+        languages: [
+            'JavaScript', 'HTML', 'CSS'
+        ],
+        fandls: [
+            'React.js', 'Node.js', 'Git', 'MongoDB', 'MySQL', 'jQuery'
+        ],
+        deployment: [
+            'Heroku', 'AWS'
+        ]
+    }
+}
---
+++ portfolio/src/components/index.js
@@ -0,0 +1,11 @@
+import IntroSection from './IntroSection';
+import ProjectsSection from './ProjectsSection';
+import Section from './Section';
+import SkillsSection from './SkillsSection';
+
+export {
+    IntroSection,
+    ProjectsSection,
+    Section,
+    SkillsSection
+}
