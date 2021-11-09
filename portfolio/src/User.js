1afb104c758511640869e7fc5284c0d41b71657f
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-11-09 15:55:24 -0600

    fixes to favicon, added footer section, modified user for testing

--- portfolio/src/User.js
+++ portfolio/src/User.js
@@ -11,25 +11,25 @@ export default {
     resumeLink: 'https://github.com/cikennedy/updated-portfolio/raw/main/assets/Resume.pdf',
     projects: [
         {
-            image: '',
-            title: '',
-            description: '',
-            projectgithub: '',
-            projectdeployed: ''
+            image: 'https://github.com/cikennedy/workerbee/raw/main/assets/SignIn.png',
+            title: 'Workerbee',
+            description: 'A MERN stack application in development that exists to help pair chore-level laborers with those in need of help.',
+            projectgithub: 'https://github.com/cikennedy/workerbee',
+            projectdeployed: 'https://workerbeeapp.herokuapp.com/'
         },
         {
-            image: '',
-            title: '',
-            description: '',
-            projectgithub: '',
-            projectdeployed: ''
+            image: 'https://tul.imgix.net/content/general/puzzle.jpg?auto=format,compress&w=1200&h=630&fit=crop',
+            title: 'Camera Puzzle',
+            description: 'A game that creates a jigsaw puzzle using the camera on a device. Uses HTML, JavaScript, MySQL, and PHP.',
+            projectgithub: 'https://github.com/cikennedy/jigsaw-cam-game',
+            projectdeployed: 'https://cikennedy.github.io/jigsaw-cam-game/'
         },
         {
-            image: '',
-            title: '',
-            description: '',
-            projectgithub: '',
-            projectdeployed: ''
+            image: 'https://github.com/cikennedy/findmypet/raw/main/assets/Screenshot.png',
+            title: 'Find My Pet',
+            description: 'Application designed to help find lost pets. Utilizes a SQL database, JavaScript, Express, and Node.',
+            projectgithub: 'https://github.com/cikennedy/findmypet',
+            projectdeployed: 'https://findmymissingpet.herokuapp.com/dashboard'
         },
         {
             image: '',
