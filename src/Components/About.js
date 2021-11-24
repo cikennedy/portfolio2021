722a8ee1de110c1ee1b8f4831df11b126e280253
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-11-23 18:20:47 -0600

    updated about section to read better on mobile

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,8 +6,8 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            Chris Kennedy
-            <br className="hidden lg:inline-block" />Software Engineer
+            About Me
+            <br className="hidden lg:inline-block" />
           </h1>
           <p className="mb-8 leading-relaxed">
             Hi! I'm a software engineer specializing in web development. I have most recently created full-stack projects using the MERN stack 
