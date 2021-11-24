80be5b42803d03ac9f9ea238979e3c44f8540d1d
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-11-24 16:59:15 -0600

    hamburger menu functionality

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { useState } from 'react';
 
 export default function Navbar() {
+
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
@@ -12,24 +20,58 @@ export default function Navbar() {
             Web Developer | Software Engineer
           </p>
         </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+
+        <button
+          className=' inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none'
+          onClick={handleClick}
+        >
+          <svg
+            className='w-6 h-6'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth={2}
+              d='M4 6h16M4 12h16M4 18h16'
+            />
+          </svg>
+          </button>
+          <div
+          className={`${
+            active ? '' : 'hidden'
+          }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+        >
+        {/* <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
           <a href="#projects" className="mr-5 hover:text-white">
             Past Work
           </a>
           <a href="#skills" className="mr-5 hover:text-white">
             Skills
           </a>
- 
-        </nav>
+          
+        </nav> */}
+        <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
+        <a href="#projects" className="mr-5 hover:text-white inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0">
+            Past Work
+          </a>
+          <a href="#skills" className="mr-5 hover:text-white inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0">
+            Skills
+          </a>
         <a href="https://github.com/cikennedy/updated-portfolio/raw/main/assets/Resume.pdf"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+          className="mr-5 hover:text-white inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0">
             Resume
           </a>
         <a
           href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+          className="mr-5 hover:text-white inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0">
           Contact Me
         </a>
+      </div>
+      </div>
       </div>
     </header>
   );
